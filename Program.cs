d44a9b27ae2034c4353e306d48ff35e47766fd72
Alex Dz <avdzyaduk@gmail.com>
2022-11-29 19:41:01 +0300

    program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] SearchForElements(string[] array, int stringLenght)
+{
+    string resultString = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= stringLenght) resultString += array[i] + " ";
+    }
+    string[] newArray = resultString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    return newArray;
+}
+
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+string input = (Console.ReadLine()!);
+int stringLenght = 3;
+string[] array = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"Введенный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Новый массив: [{string.Join(", ", SearchForElements(array, stringLenght))}]");
